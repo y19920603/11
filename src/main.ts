b26a7abba74b66420d0def3c74123a80ea6d48d0
byrorn <y19920603@yahoo.com.tw>
2025-04-07 23:00:26 +0800

    123123

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ import "uno.css";
 import "animate.css";
 import "default-passive-events";
 import "@mdi/font/css/materialdesignicons.css";
+import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
+import "datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css";
+import "datatables.net-buttons-dt/css/buttons.dataTables.min.css";
+import "datatables.net-dt/css/dataTables.dataTables.min.css";
 
 const app = createApp(App);
 app.use(setupPlugins);
